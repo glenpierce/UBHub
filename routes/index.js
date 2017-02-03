7085a610e57c3619273a354fa0f1a1c3c5dc8f20
glenpierce <glen.piercev@gmail.com>
2017-02-02 18:33:42 -0800

    input fields in jade

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'my-secret-pw',
-        database : 'epistemolog'
-    });
+    // var connection = mysql.createConnection({
+    //     host     : 'localhost',
+    //     user     : 'root',
+    //     password : 'my-secret-pw',
+    //     database : 'epistemolog'
+    // });
+    //
+    // connection.connect();
+    //
+    //   var query = connection.query('SELECT * FROM Users',function(err, rows){
+    //       if(err)
+    //           console.log("Error Selecting : %s ", err );
+    //
+    //       for (var i in rows) {
+    //           console.log(rows[i].email);
+    //       }
 
-    connection.connect();
+          res.render('index',{title:"The thing I'm working on"});
+      // });
 
-      var query = connection.query('SELECT * FROM Users',function(err, rows){
-          if(err)
-              console.log("Error Selecting : %s ", err );
-
-          for (var i in rows) {
-              console.log(rows[i].email);
-          }
-
-          res.render('index',{title:"Users - Node.js", data:rows});
-      });
-
-    connection.end();
+    // connection.end();
 });
 
 module.exports = router;
