05afb14b7a5f3a2f55f181a48e1606d822882655
glenpierce <glen.piercev@gmail.com>
2017-02-04 20:01:41 -0800

    http get requests work

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  console.log("responding")
+});
+
+router.get('login', function(req, res, next) {
+    //check password
+    //if correct
+    //res.send();
+    res.send('respond with a resource');
+    console.log("responding");
 });
 
 module.exports = router;
