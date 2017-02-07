eb6e67791d8d370ff152ffb8465afee1a8f3283c
glenpierce <glen.piercev@gmail.com>
2017-02-07 08:00:02 -0800

    http post request works

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  console.log("responding")
+  res.send('respond with req');
+});
+
+router.post('/', function(req, res){
+    console.log('POST request received');
+    console.log(req.body);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('server says: post request received');
 });
 
 router.get('login', function(req, res, next) {
     //check password
     //if correct
     //res.send();
-    res.send('respond with a resource');
-    console.log("responding");
+    // res.send('respond with a resource' + req);
+    // console.log("responding" + req);
 });
 
 module.exports = router;
