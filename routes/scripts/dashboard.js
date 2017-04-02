94f3698f6b2a2c1b0dbbee8035991c62b8b3f74f
Glen Pierce <glen@jostle.me>
2017-04-01 18:34:39 -0700

    CSS works.

--- routes/scripts/dashboard.js
+++ routes/scripts/dashboard.js
@@ -3,3 +3,11 @@ window.alert("dashboard.js");
 var loadData = function(){
 
 };
+
+var fadeButtonsIn = function () {
+    $("button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+}
