99383a5cabff75a2fad0b6dabab1870f4c9e4ae8
Glen Pierce <glen@jostle.me>
2017-05-20 16:26:46 -0700

    Map adds locations.

--- routes/map.js
+++ routes/map.js
@@ -17,6 +17,63 @@ app.use(session({
     activeDuration: 5 * 60 * 1000
 }));
 
+var getLatLong = function (address){
+    if(address){
+        var addressQueryString = address.replace(/\s+/g, "+");
+        //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs
+        var options = {
+            host: 'maps.googleapis.com',
+            path: '/maps/api/geocode/json?address=' + addressQueryString + "&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs",
+            //since we are listening on a custom port, we need to specify it by hand
+            //port: '1337',
+            method: 'GET'
+            // useQuerystring: true,
+            // qs: 'address=' + "1600+Amphitheatre+Parkway,+Mountain+View,+CA" + "&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs"
+        };
+
+        var req = https.request(options, function(response) {
+            // console.log(response);
+            var data = '';
+            response.on('data', function(chunk) {
+                data += chunk;
+            });
+            response.on('end', function() {
+                var result = JSON.parse(data);
+                lat = result.results[0].geometry.location.lat;
+                lng = result.results[0].geometry.location.lng;
+                console.log(lat);
+                console.log(lng);
+                createMarker("name", "address", lat, lng, "type");
+            });
+        });
+        req.end();
+    } else {
+        console.log("missing address");
+    }
+};
+
+var createMarker = function (name, address, lat, lng, type){
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: config.rdsHost,
+        user: config.rdsUser,
+        password: config.rdsPassword,
+        database: config.rdsDatabase
+    });
+
+    connection.connect();
+    var query = 'CALL createMarker("' + name + '", "' + address + '", "' + lat + '", "' + lng + '", "' + type + '")';
+    console.log(query);
+    connection.query(query, function(err, rows, fields) {
+        if (!err) {
+            console.log("marker created");
+        } else {
+            console.log(err);
+        }
+    });
+    connection.end();
+};
+
 router.get('/', function(req, res, next) {
     var mapData = "";
 
@@ -33,17 +90,8 @@ router.get('/', function(req, res, next) {
         if (!err) {
             // console.log('The user db contains: ', rows);
             mapData = rows;
-            console.log(mapData);
             if (req.session && req.session.user) {
                 res.render('map', {mapData:JSON.stringify(mapData)});
-                rows.forEach(function (element){
-                    // console.log(element);
-                    if(element.lat && element.lng){
-                        console.log(element.lat + ", " + element.lng);
-                    } else {
-                        console.log("missing lat/lng");
-                    }
-                });
             } else {
                 console.log("not logged in");
                 req.session.reset();
@@ -71,38 +119,8 @@ router.get('/update', function(req, res, next) {
     connection.query('SELECT * from locations where address="Monaco"', function(err, rows, fields) {
         if (!err) {
             mapData = rows;
-            // console.log(mapData);
                 rows.forEach(function (element){
-                    if(element.address){
-                        //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs
-                        var options = {
-                            host: 'maps.googleapis.com',
-                            path: '/maps/api/geocode/json?address=' + "1600+Amphitheatre+Parkway,+Mountain+View,+CA" + "&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs",
-                            //since we are listening on a custom port, we need to specify it by hand
-                            //port: '1337',
-                            method: 'GET'
-                            // useQuerystring: true,
-                            // qs: 'address=' + "1600+Amphitheatre+Parkway,+Mountain+View,+CA" + "&key=AIzaSyAEKjvE48-VV37P2pGBWFphvlrx8BXGDCs"
-                        };
-
-                        var req = https.request(options, function(response) {
-                            // console.log(response);
-                            var data = '';
-                            response.on('data', function(chunk) {
-                                data += chunk;
-                            });
-                            response.on('end', function() {
-                                var result = JSON.parse(data);
-                                lat = result.results[0].geometry.location.lat;
-                                lng = result.results[0].geometry.location.lng;
-                                console.log(lat);
-                                console.log(lng);
-                            });
-                        });
-                        req.end();
-                    } else {
-                        console.log("missing lat/lng");
-                    }
+                    getLatLong(element.address);
                 });
         } else {
             console.log('Error while performing Query.');
@@ -113,6 +131,24 @@ router.get('/update', function(req, res, next) {
 
 router.post('/', function (req, res) {
     console.log(req.body);
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: config.rdsHost,
+        user: config.rdsUser,
+        password: config.rdsPassword,
+        database: config.rdsDatabase
+    });
+
+    connection.connect();
+    connection.query('CALL createLocation("' + req.body.location + '", "' + req.body.title + '")', function(err, rows, fields) {
+        if (!err) {
+            console.log("location created");
+            getLatLong(req.body.location);
+        } else {
+            console.log(err);
+        }
+    });
+    connection.end();
     res.redirect('back');
 });
 
