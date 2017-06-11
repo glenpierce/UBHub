05c31229af29405bbcda5d8784b49a92870659a4
Glen Pierce <glen@jostle.me>
2017-06-10 18:03:51 -0700

    Editing editupload

--- routes/editUpload.js
+++ routes/editUpload.js
@@ -4,6 +4,32 @@ var mysql = require('mysql');
 
 var config = require('../config.js');
 
+router.get('/', function(req, res, next) {
+    connection = mysql.createConnection({
+        host: config.rdsHost,
+        user: config.rdsUser,
+        password: config.rdsPassword,
+        database: config.rdsDatabase
+    });
+
+    var id = req.query.id;
+
+    connection.connect();
+    query = 'CALL getUploadByUserById("' + req.session.user + '", "' + id + '")';
+    console.log(query);
+    connection.query(query, function(err, rows, fields) {
+        if (!err) {
+            // console.log({data:rows[0]});
+            data = JSON.stringify(rows[0][0]);
+            console.log(data);
+            res.render('editUpload', {fromServer:data, username: req.session.user});
+        } else {
+            console.log(err);
+        }
+    });
+    connection.end();
+});
+
 router.post('/', function(req, res){
 
     console.log("edit upload");
