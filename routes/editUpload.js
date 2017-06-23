6d8cad89d46603e73649fd3ac67b42d81c666d7d
Glen Pierce <glen@jostle.me>
2017-06-22 20:31:42 -0700

    Editing editupload

--- routes/editUpload.js
+++ routes/editUpload.js
@@ -4,6 +4,8 @@ var mysql = require('mysql');
 
 var config = require('../config.js');
 
+var id;
+
 router.get('/', function(req, res, next) {
     connection = mysql.createConnection({
         host: config.rdsHost,
@@ -12,7 +14,7 @@ router.get('/', function(req, res, next) {
         database: config.rdsDatabase
     });
 
-    var id = req.query.id;
+    id = req.query.id;
 
     connection.connect();
     query = 'CALL getUploadByUserById("' + req.session.user + '", "' + id + '")';
@@ -44,12 +46,17 @@ router.post('/', function(req, res){
 
     connection.connect();
 
-    connection.query('CALL updateLocation("' + req.body.locationId + '")', function(err, rows, fields){
+    query = 'CALL getUploadById("' + id + '")';
+    query = 'SELECT * FROM locations WHERE id = ' + id + ';';
+    console.log(query);
+    connection.query(query, function(err, rows, fields){
         if (!err) {
-            if(req.body.username == rows.user){
-                data = JSON.stringify(rows[0]);
+            if(req.body.username === rows.user){
+                updateUpload(req, res);
+                console.log("correct user");
             } else {
-                wrongUser = false;
+                res.redirect('login');
+                console.log("wrong user");
             }
 
         } else {
@@ -58,12 +65,31 @@ router.post('/', function(req, res){
     });
 
     connection.end();
-
-    if(wrongUser){
-        res.redirect('login');
-    } else {
-        res.render('editUpload', {fromServer: data});
-    }
 });
+
+function updateUpload(req, res){
+    var query = 'CALL updateUpload("' + req.body.locationId + '", "' + req.body.location + '", "' + req.body.title + '", "' + req.session.user + '", "' + req.body.location + '", "' + req.body.scale + '", \'' + JSON.stringify(req.body) + '\')';
+    query = 'UPDATE locations SET myJson = \'' + JSON.stringify(req.body) + '\' WHERE id = ' + id + ';';
+    console.log(query);
+
+    var connection = mysql.createConnection({
+        host: config.rdsHost,
+        user: config.rdsUser,
+        password: config.rdsPassword,
+        database: config.rdsDatabase
+    });
+
+    connection.connect();
+
+    connection.query(query, function(err, rows, fields){
+        if (!err) {
+            res.redirect('yourUploads');
+        } else {
+            console.log('Error while performing Query.');
+        }
+    });
+
+    connection.end();
+}
 
 module.exports = router;
