dfa976ce257cb89f803f63eeae54136a2b93bebb
Glen Pierce <glen@jostle.me>
2018-03-03 13:31:15 -0800

    dashboard updates. Getting Promises working for data retrieval.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -15,25 +15,204 @@ app.use(session({
     activeDuration: 5 * 60 * 1000
 }));
 
-router.get('/', function(req, res, next) {
-    console.log(req.query.id);
-    if (req.session && req.session.user) {
-        if(req.query.id){
-            query = `Call selectSiteForUserAndReturnIt('${req.session.user}', '${req.query.id}')`;
-        } else {
-            query = `Call getSelectedSiteByUserQuery('${req.session.user}')`;
-        }
-        render = function (rows) {
-            console.log(rows);
-            res.render('dashboard', {rows: rows});
-        };
-        makeDbCall(query, render);
-    } else {
-        console.log("not logged in");
-        req.session.reset();
-        res.redirect('/');
+router.get('/', function(req, res, next){
+
+    var userData = {};
+
+    function getSelectedSite() {
+        return new Promise(function (resolve, reject) {
+            if (req.session && req.session.user) {
+                if (req.query.id) {
+                    query = `Call selectSiteForUserAndReturnIt('${req.session.user}', '${req.query.id}')`;
+                } else {
+                    query = `Call getSelectedSiteByUserQuery('${req.session.user}')`;
+                }
+                connection = mysql.createConnection({
+                    host: config.rdsHost,
+                    user: config.rdsUser,
+                    password: config.rdsPassword,
+                    database: config.rdsDatabase
+                });
+
+                connection.connect();
+                console.log(query);
+                connection.query(query, function (err, rows, fields) {
+                    if (!err) {
+                        // console.log(rows);
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.');
+                        console.log(err.code);
+                        console.log(err.message);
+                        reject(err);
+                    }
+                });
+                connection.end();
+            } else {
+                reject();
+            }
+        });
     }
+
+    function getPrograms(){
+        return new Promise(function(resolve, reject) {
+            query = "";
+            if (req.session && req.session.user) {
+                connection = mysql.createConnection({
+                    host: config.rdsHost,
+                    user: config.rdsUser,
+                    password: config.rdsPassword,
+                    database: config.rdsDatabase
+                });
+
+                connection.connect();
+                console.log(query);
+                connection.query(query, function(err, rows, fields) {
+                    if (!err) {
+                        console.log(rows);
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.');
+                        console.log(err.code);
+                        console.log(err.message);
+                        reject(err);
+                    }
+                });
+                connection.end();
+            } else {
+                reject();
+            }
+        });
+    }
+
+    function getCategories() {
+        return new Promise(function (resolve, reject) {
+            query = "";
+            if (req.session && req.session.user) {
+                connection = mysql.createConnection({
+                    host: config.rdsHost,
+                    user: config.rdsUser,
+                    password: config.rdsPassword,
+                    database: config.rdsDatabase
+                });
+
+                connection.connect();
+                console.log(query);
+                connection.query(query, function (err, rows, fields) {
+                    if (!err) {
+                        console.log(rows);
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.');
+                        console.log(err.code);
+                        console.log(err.message);
+                        reject(err);
+                    }
+                });
+                connection.end();
+            } else {
+                reject();
+            }
+        });
+    }
+
+    function getIndicators() {
+        return new Promise(function (resolve, reject) {
+            query = "";
+            if (req.session && req.session.user) {
+                connection = mysql.createConnection({
+                    host: config.rdsHost,
+                    user: config.rdsUser,
+                    password: config.rdsPassword,
+                    database: config.rdsDatabase
+                });
+
+                connection.connect();
+                console.log(query);
+                connection.query(query, function (err, rows, fields) {
+                    if (!err) {
+                        console.log(rows);
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.');
+                        console.log(err.code);
+                        console.log(err.message);
+                        reject(err);
+                    }
+                });
+                connection.end();
+            } else {
+                reject();
+            }
+        });
+    }
+
+    function getUserData(siteId){
+        return new Promise(function(resolve, reject) {
+            query = "select * from userData where site = " + siteId;
+            if (req.session && req.session.user) {
+                connection = mysql.createConnection({
+                    host: config.rdsHost,
+                    user: config.rdsUser,
+                    password: config.rdsPassword,
+                    database: config.rdsDatabase
+                });
+
+                connection.connect();
+                console.log(query);
+                connection.query(query, function(err, rows, fields) {
+                    if (!err) {
+                        console.log(rows);
+                        resolve(rows);
+                    } else {
+                        console.log('Error while performing Query.');
+                        console.log(err.code);
+                        console.log(err.message);
+                        reject(err);
+                    }
+                });
+                connection.end();
+            } else {
+                reject();
+            }
+        });
+    }
+
+    getSelectedSite().then(function (values) {
+        console.log(values[0][0].siteName);
+        getUserData(values[0][0].id);
+        res.render('dashboard');
+    }).catch(function(error) {
+            console.log(error);
+            res.render('dashboard');
+        }
+    );
+
+    // Promise.all([getSelectedSite, getPrograms, getCategories, getIndicators, getUserData]).then(function(values) {
+    //     console.log(values);
+    // });
+
 });
+
+// router.get('/', function(req, res, next) {
+//     console.log(req.query.id);
+//     if (req.session && req.session.user) {
+//         if(req.query.id){
+//             query = `Call selectSiteForUserAndReturnIt('${req.session.user}', '${req.query.id}')`;
+//         } else {
+//             query = `Call getSelectedSiteByUserQuery('${req.session.user}')`;
+//         }
+//         render = function (rows) {
+//             console.log(rows);
+//             res.render('dashboard', {rows: rows});
+//         };
+//         makeDbCall(query, render);
+//     } else {
+//         console.log("not logged in");
+//         req.session.reset();
+//         res.redirect('/');
+//     }
+// });
 
 makeDbCall = function(queryString, callback){
     connection = mysql.createConnection({
