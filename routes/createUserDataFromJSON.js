049b3a0d41e82e1213ec81c6ddb3cd3566e68a3c
Glen Pierce <glen.piercev@gmail.com>
2019-06-23 19:51:15 -0700

    UBIF-0021 Save data #82.

--- routes/createUserDataFromJSON.js
+++ routes/createUserDataFromJSON.js
@@ -27,7 +27,7 @@ router.post('/', function(req, res){
                 id = Date.now();
                 queryString = "INSERT INTO userData (site, program, jsonData, id) VALUES ('" + selectedSite[0][0].id + "', '" + 1 + "', '" + data + "', " + id + ");";
             } else {
-                queryString = "update userData set jsonData = '" + data + "' where id = " + id + ");";
+                queryString = "update userData set jsonData = '" + data + "' where id = " + id + ";";
             }
             makeDbCallAsPromise(queryString).then(() => {
                 let response = id.toString();
