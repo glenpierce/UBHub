a99d01d4213c9109219e1a7974bcb3787bd687d1
Glen Pierce <glen.piercev@gmail.com>
2020-09-23 18:15:56 -0700

    Adding indicators to program object retrived by program.js

--- routes/program.js
+++ routes/program.js
@@ -1,13 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var mysql = require('mysql');
-var session = require('client-sessions');
-var path = require("path");
-var fs = require("fs");
+const express = require('express');
+const router = express.Router();
+const session = require('client-sessions');
 
-var app = express();
+const app = express();
 
-var config = require('../config.js');
+const config = require('../config.js');
 
 app.use(session({
     cookieName: 'session',
@@ -28,35 +25,43 @@ router.get('/', function (req, res, next) {
                 return;
             default:
                 let programData = {};
-                let queryString = "select * from programs where id = " + req.query.newId + " and author = '" + req.session.user + "';";
+                let queryString = `select * from programs where id = '${req.query.newId}' and author = '${req.session.user}';`;
                 makeDbCallAsPromise(queryString)
-                    .then(rows => {
-                        let queryString = "select * from categories where program = " + req.query.newId + ";";
-                        makeDbCallAsPromise(queryString)
-                            .then(rows => {
-                                programData.categories = rows;
-                                let queryString = "select * from indicators where (categoryId = " + rows[0].id + ")";
-                                for(let i = 1; i < rows.length; i += 1) {
-                                    queryString += " OR (categoryId = " + rows[i].id + ")";
-                                }
-                                queryString += ";";
-                                makeDbCallAsPromise(queryString)
-                                    .then(rows => {
-                                        programData.indicators = rows;
-                                        programData = JSON.stringify(programData);
-                                        res.render('program', {username: req.session.user, id: req.query.newId, programData:programData});
-                                    });
-                            });
-                    });
+                .then(program => {
+                    let queryString = `select * from categories where program = ${req.query.newId};`;
+                    return makeDbCallAsPromise(queryString);
+                })
+                .then(categories => {
+                    programData.categories = categories;
+                    let queryString = `select * from indicatorInCategory where (categoryId = ${categories[0].id})`;
+                    for(let i = 1; i < categories.length - 1; i += 1) {
+                        queryString += ` OR (categoryId = ${categories[i].id})`;
+                    }
+                    queryString += `;`;
+                    console.log(queryString);
+                    return makeDbCallAsPromise(queryString);
+                })
+                .then(indicatorIds => {
+                    let queryString = `select * from indicators where (id = ${indicatorIds[0].indicatorTemplateId})`;
+                    for(let i = 1; i < indicatorIds.length - 1; i += 1) {
+                        queryString += ` OR (categoryId = ${indicatorIds[i].indicatorTemplateId})`;
+                    }
+                    return makeDbCallAsPromise(queryString);
+                })
+                .then(indicators => {
+                    programData.indicators = indicators;
+                    programData = JSON.stringify(programData);
+                    res.render('program', {username: req.session.user, id: req.query.newId, programData:programData});
+                });
                 return;
         }
     } else {
-        let queryString = "select * from userData where id = " + req.query.id + ";";
+        let queryString = `select * from userData where id = ${req.query.id};`;
         makeDbCallAsPromise(queryString)
             .then(rows => {
                 let jsonToSend = rows[0].jsonData;
                 if(req.session.user == rows[0].userEmail) {
-                    if(rows[0].program == "1") {
+                    if(rows[0].program == '1') {
                         res.render('ubifProgram', {username: req.session.user, id: req.query.id, dataFromServer:jsonToSend});
                     } else {
                         res.render('program', {
@@ -66,7 +71,7 @@ router.get('/', function (req, res, next) {
                         });
                     }
                 } else {
-                    console.log("user mismatch in program.js: " + req.session.user + " " + rows[0]);
+                    console.log(`user mismatch in program.js: ${req.session.user} ${rows[0]}`);
                 }
             });
         //get userData by id, confirm user email from db matches authenticated user making request
