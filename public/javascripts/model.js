ae675ab88b17a8b36e2fbab0ce185bf1ef173dd1
Glen Pierce <glen.piercev@gmail.com>
2021-01-10 15:48:52 -0800

    Using the model to create a Program instance on the front end to better use as a data structure so we can actually save these things.

--- public/javascripts/model.js
+++ public/javascripts/model.js
@@ -3,4 +3,18 @@
         console.log(myParameter);
       };
 
+      exports.Program = function () {
+          this.create = function(data) {
+              this.categories = [];
+              for(let i = 0; i < data.categories.length; i += 1) {
+                  this.categories.push(data.categories[i])
+                  for (let j = 0; j < data.indicators.length; j += 1) {
+                      if (data.indicatorIds[j].categoryId == data.categories[i].id) {
+                          this.categories[i].indicators = {id:data.indicatorIds[j]};
+                      }
+                  }
+              }
+          }
+      }
+
 })(typeof exports === 'undefined'? this['model']={}: exports); //source: https://stackoverflow.com/questions/3225251/how-can-i-share-code-between-node-js-and-the-browser
