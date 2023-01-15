8f4cc6b56856e2b9d5a43013bf9c40920c537de0
Glen Pierce <glen.piercev@gmail.com>
2023-01-15 15:06:59 -0800

    adding Eric's bio and removing old aboutUs.html

--- nodeServer/routes/aboutUs.js
+++ nodeServer/routes/aboutUs.js
@@ -1,17 +1,8 @@
-var express = require('express');
-var mysql = require('mysql');
-var router = express.Router();
-var path = require("path");
-var request = require('request');
-var config = require('../config.js');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    // if(req.session && req.session.user)
-    //     return res.redirect('indicators');
-    // else
-        res.render('aboutUs', {username: req.session.user});
+    res.render('aboutUs', {username: req.session.user});
 });
-
 
 module.exports = router;
