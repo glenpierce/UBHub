b8edcced5b4fc05368aad61bfab591001b4c8237
Glen Pierce <glen.piercev@gmail.com>
2023-07-26 20:52:55 -0700

    initial script

--- nodeServer/configure_amz_linux_app.sh
+++ nodeServer/configure_amz_linux_app.sh
@@ -1,7 +1,21 @@
-echo "hello cdk"
+echo "HELLO CDK ---BEGINNING OF CONFIGURATION SCRIPT---"
 
-curl --silent --location https://rpm.nodesource.com/setup_19.5.0 | bash -
-yum -y install nodejs
+sudo yum install gcc-c++ make
+
+curl -O https://nodejs.org/dist/v19.5.0/node-v19.5.0-linux-arm64.tar.gz
+
+tar -xzf node-v19.5.0-linux-arm64.tar.gz
+
+sudo mv node-v19.5.0-linux-arm64 /usr/local/node
+
+sudo ln -s /usr/local/node/bin/node /usr/local/bin/node
+sudo ln -s /usr/local/node/bin/npm /usr/local/bin/npm
+
+node -v
+
+sudo yum install -y npm
+
+npm -v
 
 echo "$1"
 
