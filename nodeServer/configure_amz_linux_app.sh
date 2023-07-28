61a615a23f6f682be602aa0eaff62124430f0283
Glen Pierce <glen.piercev@gmail.com>
2023-07-27 22:41:01 -0700

    The highest level of node we can get is 16 because of limits on AWS Linux 2 ec2s... they also seem to be recommending nvm for install here: https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html

--- nodeServer/configure_amz_linux_app.sh
+++ nodeServer/configure_amz_linux_app.sh
@@ -1,19 +1,30 @@
 echo "HELLO CDK ---BEGINNING OF CONFIGURATION SCRIPT---"
 
-sudo yum install gcc-c++ make
+sudo yum update
 
-curl -O https://nodejs.org/dist/v19.5.0/node-v19.5.0-linux-arm64.tar.gz
+#sudo yum install gcc-c++ make
+#
+#curl -O https://nodejs.org/dist/v19.5.0/node-v19.5.0-linux-x64.tar.xz
+#
+#tar -xf node-v19.5.0-linux-x64.tar.xz
+#
+#sudo mv node-v19.5.0-linux-x64 bin/node
+#
+#chmod +x ~/bin/node/bin/node
 
-tar -xzf node-v19.5.0-linux-arm64.tar.gz
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-sudo mv node-v19.5.0-linux-arm64 /usr/local/node
+. ~/.nvm/nvm.sh
 
-sudo ln -s /usr/local/node/bin/node /usr/local/bin/node
-sudo ln -s /usr/local/node/bin/npm /usr/local/bin/npm
+#nvm install 19.5.0
+
+nvm install 16 # Amazon Linux 2 does not currently support the current LTS release (version 18.x) of Node.js. Use Node.js version 16.x with this command instead.
 
 node -v
 
-sudo yum install -y npm
+#sudo yum install -y npm
+
+nvm install-latest-npm
 
 npm -v
 
