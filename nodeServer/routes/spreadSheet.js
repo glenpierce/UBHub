95fcc695a09b45678151a008e936fb11516aab11
glenpierce <glen.piercev@gmail.com>
2025-12-14 22:07:20 -0800

    Correct spreadSheet.js router to refer to correct view

--- nodeServer/routes/spreadSheet.js
+++ nodeServer/routes/spreadSheet.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('spreadsheet', {
+    res.render('spreadSheet', {
         tables: ['indicators', 'categories', 'indicatorValues']
     });
 });
